8b94c9e5c734aa80f1c328d40012d3393dea2191
Mustafa <mustafaerbilici@gmail.com>
2025-08-12 15:36:57 +0300

    Refactor ImageSlider to fetch data from API

--- frontend/src/components/ImageSlider.jsx
+++ frontend/src/components/ImageSlider.jsx
@@ -1,57 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import reviewService from '../services/reviewService';
 import '../styles/ImageSlider.css';
 
-const panels = [
-  {
-    id: 1,
-    image: 'https://image.tmdb.org/t/p/w500/sjMN7DRi4sGiledsmllEw5HJjPy.jpg',
-    title: 'Inception',
-    rate: '⭐ 4.8',
-    director: 'Director: Christopher Nolan.',
-  },
-  {
-    id: 2,
-    image: 'https://image.tmdb.org/t/p/w500/7tvAnzZj9e9AjdoHaN9jshm2Cjw.jpg',
-    title: 'Interstellar',
-    rate: '⭐ 4.7',
-    director: 'Director: Christopher Nolan.',
-  },
-  {
-    id: 3,
-    image: 'https://image.tmdb.org/t/p/w500/aosm8NMQ3UyoBVpSxyimorCQykC.jpg',
-    title: 'The Matrix',
-    rate: '⭐ 4.6',
-    director: 'Director: Christopher Nolan.',
-  },
-  {
-    id: 4,
-    image: 'https://image.tmdb.org/t/p/w500/4YZpsylmjHbqeWzjKpUEF8gcLNW.jpg',
-    title: 'Parasite',
-    rate: '⭐ 4.5',
-    director: 'Director: Christopher Nolan.',
-  },
-  {
-    id: 5,
-    image: 'https://image.tmdb.org/t/p/w500/mubt4bnVfpJ5lBMq93DidEuMkJr.jpg',
-    title: 'Whiplash',
-    rate: '⭐ 4.4',
-    director: 'Director: Christopher Nolan.',
-  },
-];
-
 const ImageSlider = () => {
-  const repeatedPanels = [...panels, ...panels, ...panels, ...panels];
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await reviewService.getDailyReviews();
+        setData(response);
+      } catch (error) {
+        console.error('API data fetch error:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const repeatedPanels = [...data, ...data, ...data, ...data];
 
   return (
     <div className="slider-container">
       <div className="slider-track">
-        {repeatedPanels.map((panel, index) => (
-          <div className="slider-card" key={index}>
-            <img src={panel.image} alt={panel.title} />
-            <div className="title">{panel.title}</div>
-            <div className="rate">{panel.rate}</div>
-            <div className="director">{panel.director}</div>
-          </div>
+        {repeatedPanels.map((panel) => (
+          <Link to={`/movies/${panel.movie.tmdbID}/${panel.movie.title}`} className="slider-card" key={panel.movie.tmdbID}>
+            <img src={panel.movie.poster} alt={panel.movie.title} />
+            <div className="title">{panel.movie.title}</div>
+            <div className="rate">{panel.movie.rating}</div>
+            <div className="director">{panel.movie.director}</div>
+          </Link>
         ))}
       </div>
     </div>
